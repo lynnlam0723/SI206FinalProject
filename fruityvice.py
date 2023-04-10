7f2e8c64edd1032bee75a46f73156ebc0c909a55
Lynn <lynnnlam0723@gmail.com>
2023-04-10 15:24:16 -0400

    loaded fruityvice data into a database

--- fruityvice.py
+++ fruityvice.py
@@ -28,6 +28,9 @@ def gather_data():
     #load data and create databases
     load_data(cur, conn)
 
+    #close connection
+    conn.close()
+
 def load_data(cur, conn):
     #get data from API
     response = requests.get("https://fruityvice.com/api/fruit/all")
@@ -37,21 +40,25 @@ def load_data(cur, conn):
 
     #create genus table
     create_genus_db(data, cur, conn)
+    cont_genus_db(data, cur, conn)
+
     #create family table
     create_family_db(data, cur, conn)
-    #create full fruits table
+    cont_family_db(data, cur, conn)
+    
+    #create fruits table
     create_fruit_db(data, cur, conn)
+    continue_loading_fruits(data, cur, conn)
 
 def create_fruit_db(data, cur, conn):
     #create a table named Fruits
-    cur.execute("DROP TABLE IF EXISTS Fruits")
-    cur.execute("CREATE TABLE Fruits (id INTEGER PRIMARY KEY, genus INTEGER, family INTEGER, name TEXT, carbs REAL, protein REAL, fat REAL, calories REAL, sugar REAL)")
+    cur.execute("CREATE TABLE IF NOT EXISTS Fruits (id INTEGER PRIMARY KEY, genus INTEGER, family INTEGER, name TEXT, carbs REAL, protein REAL, fat REAL, calories REAL, sugar REAL)")
 
     #begin loading data into table
-    counter = 0
+    counter = 1
     #loop through json list
     for item in data:
-        nutrition = item["nutrition"]
+        nutrition = item["nutritions"]
         
         #get fruit's genus
         cur.execute("SELECT id FROM Genus WHERE name = ?", (item["genus"],))
@@ -73,30 +80,76 @@ def create_fruit_db(data, cur, conn):
     #commit changes
     conn.commit()
 
+def continue_loading_fruits(data, cur, conn):
+    # continue inserting until end of fruity file
+    for i in range(25, len(data)):
+        nutrition = data[i]["nutritions"]
+        
+        #get fruit's genus
+        cur.execute("SELECT id FROM Genus WHERE name = ?", (data[i]["genus"],))
+        g = cur.fetchone()[0]
+        #get fruit's family
+        cur.execute("SELECT id FROM Families WHERE name = ?", (data[i]["family"],))
+        f = cur.fetchone()[0]
+
+        fruit = (data[i]["id"], g, f, data[i]["name"], nutrition["carbohydrates"], nutrition["protein"], nutrition["fat"], nutrition["calories"], nutrition["sugar"])
+        cur.execute("INSERT INTO Fruits VALUES (?,?,?,?,?,?,?,?,?)", fruit)
+    
+    #commit changes
+    conn.commit()
+
 def create_genus_db(data, cur, conn):
-    cur.execute("DROP TABLE IF EXISTS Genus")
     #create a table named Genus
-    cur.execute("CREATE TABLE Genus (id INTEGER PRIMARY KEY, name TEXT)")
+    cur.execute("CREATE TABLE IF NOT EXISTS Genus (id INTEGER PRIMARY KEY, name TEXT)")
 
     #begin loading data into a table
     #use insert or ignore to get unique
     counter = 1 # <- tracks which ID we are on
+    limit =  1 # <- tracks how many are inserted at this time
     for fruit in data:
-        cur.execute("INSERT OR IGNORE INTO Genus VALUES (?,?)", (counter, fruit["genus"]))
+        cur.execute("INSERT UNIQUE INTO Genus VALUES (?,?)", (counter, fruit["genus"]))
+        counter += 1 #how to increment counter only when inserted? or does it not matter?
+
+        limit += 1
+        if limit > 25:
+            break
+
+    #commit changes
+    conn.commit()
+
+def cont_genus_db(data, cur, conn):
+    #continue loading values of genuses
+    counter = 1
+    for i in range(25, len(data)):
+        cur.execute("INSERT UNIQUE INTO Genus VALUES (?,?)", (counter, data[i]["genus"]))
         counter += 1 #how to increment counter only when inserted? or does it not matter?
 
     #commit changes
     conn.commit()
 
 def create_family_db(data, cur, conn):
-    cur.execute("DROP TABLE IF EXISTS Families")
     #create a table named Families
-    cur.execute("CREATE TABLE Families (id INTEGER PRIMARY KEY, name TEXT)")
+    cur.execute("CREATE TABLE IF NOT EXISTS Families (id INTEGER PRIMARY KEY, name TEXT)")
 
     #begin loading data into table
     counter = 1
+    limit = 1
     for fruit in data:
         cur.execute("INSERT OR IGNORE INTO Families VALUES (?,?)", (counter, fruit["family"]))
+        counter += 1 #how to increment counter only when inserted? or does it not matter?
+
+        limit += 1
+        if limit > 25:
+            break
+
+    #commit changes
+    conn.commit()
+
+def cont_family_db(data, cur, conn):
+    #continue loading values of genuses
+    counter = 1
+    for i in range(25, len(data)):
+        cur.execute("INSERT UNIQUE INTO Genus VALUES (?,?)", (counter, data[i]["family"]))
         counter += 1 #how to increment counter only when inserted? or does it not matter?
 
     #commit changes
@@ -105,6 +158,8 @@ def create_family_db(data, cur, conn):
 #def calc_data():
 
 
-
+if __name__ == '__main__':
+    gather_data()
+    
 
 
