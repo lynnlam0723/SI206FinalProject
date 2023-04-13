ec4578fcedd77bd6799dbb4c958c7c197ca3a6ef
Lynn <lynnnlam0723@gmail.com>
2023-04-13 15:37:02 -0400

    finished everything yippie

--- beer.py
+++ beer.py
@@ -2,6 +2,8 @@ import sqlite3
 import json
 import os
 import requests
+import matplotlib
+import matplotlib.pyplot as plt
 
 def gather_data(cur, conn):
     #create table
@@ -12,9 +14,9 @@ def gather_data(cur, conn):
     load_data("?page=2", cur, conn)
     load_data("?page=3", cur, conn)
     load_data("?page=4", cur, conn)
+    load_data("?page=5", cur, conn)
 
-    #close connection
-    conn.close()
+    return cur, conn
 
 def load_data(page_num, cur, conn):
     #get data from API
@@ -51,10 +53,71 @@ def create_contributed_db(data, cur, conn):
     #commit changes
     conn.commit()
 
+def visualization(data):
+    plt.figure(1)
+
+    #create x and y values
+
+    #sort by increasing abv
+    data.sort(key = lambda x: x[1])
+
+    #x = id by abv, y = id by pH
+    # abvs = create_axis_values(data, 1)
+    # phs = create_axis_values(data, 2)
+
+    x_vals, y_vals = create_axes(data)
+
+    #create and show scatterplot
+    plt.plot(x_vals, y_vals)
+    plt.show()
+    #no relation :/
+
+    #plot max abvs
+    data.sort(key = lambda x: x[1], reverse=True)
+    names = [data[0][3], data[1][3], data[2][3], data[3][3], data[4][3]]
+    abvs = [data[0][1], data[1][1], data[2][1], data[3][1], data[4][1]]
+    plt.barh(names, abvs)
+    plt.show()
+
+def create_axes(data):
+    #data is sorted by increasing abv value
+    x = list()
+    y = list()
+    for beer in data:
+        x.append(beer[1])
+        y.append(beer[2])
+
+    return x, y
+    
+def create_axis_values(data, idx):
+    axis = list()
+    for beer in data:
+        if beer[idx] not in axis:
+            axis.append(beer[idx])
+    return axis
+
 def data_calcs(cur, conn):
-    #select beers with a ph under 4
-    cur.execute("CREATE TABLE IF NOT EXISTS PHUnder4")
-    cur.execute("SELECT name FROM Beers WHERE ph < 4.0")
+    #join beers with a ph over 4.4 and abv over 8
+
+    #ph over 4.4
+    cur.execute("CREATE TABLE IF NOT EXISTS pHOver4 (id INTEGER PRIMARY KEY, name TEXT, abv REAL, ph REAL, contributed_by_id INTEGER)")
+    cur.execute("SELECT * FROM Beers WHERE ph > 4.3")
+    data = cur.fetchall()
+    for beer in data:
+        b = (beer[0], beer[1], beer[2], beer[3], beer[4])
+        cur.execute("INSERT OR IGNORE INTO pHOver4 VALUES (?,?,?,?,?)", b)
+
+    #join tables
+    #format: id, abv, ph
+    cur.execute("SELECT Beers.id, Beers.abv, pHOver4.ph, pHOver4.name FROM Beers JOIN pHOver4 ON Beers.abv > 8.0 AND Beers.id = pHOver4.id AND Beers.abv < 20.0")
+    beers = cur.fetchall()
+
+    conn.commit()
+
+    #visualize data
+    visualization(beers)
+
+    return cur, conn
 
 def main():
     #create path for database
@@ -62,7 +125,9 @@ def main():
     conn = sqlite3.connect(path+'/'+ "beer.db")
     cur = conn.cursor()
 
-    gather_data(cur, conn)
+    cur, conn = gather_data(cur, conn)
+    cur, conn = data_calcs(cur, conn)
+    conn.close()
 
 if __name__ == '__main__':
     main()
