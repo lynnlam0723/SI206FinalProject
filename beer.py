e348e878966a28c00a1ab6e8336b12f3ee4d1f2b
Lynn <lynnnlam0723@gmail.com>
2023-04-15 17:33:55 -0400

    reorganized code

--- beer.py
+++ beer.py
@@ -7,16 +7,66 @@ import matplotlib.pyplot as plt
 
 def gather_data(cur, conn):
     #create table
-    cur.execute("CREATE TABLE IF NOT EXISTS Beers (id INTEGER PRIMARY KEY, name TEXT, abv REAL, ph REAL, contributed_by_id INTEGER)")
+    cur.execute("""CREATE TABLE IF NOT EXISTS Beers (id INTEGER PRIMARY KEY, name TEXT, 
+                 abv REAL, ph REAL, contributed_by_id INTEGER)""")
 
     #load in data
-    load_data("", cur, conn)
-    load_data("?page=2", cur, conn)
-    load_data("?page=3", cur, conn)
-    load_data("?page=4", cur, conn)
-    load_data("?page=5", cur, conn)
+    cur.execute("SELECT * FROM Beers")
+    count = cur.fetchall().size()
+    if count == 0:
+        load_data("", cur, conn)
+    elif count < 26:
+        load_data("?page=2", cur, conn)
+    elif count < 51:
+        load_data("?page=3", cur, conn)
+    elif count < 76:
+        load_data("?page=4", cur, conn)
+    else:
+        load_data("?page=5", cur, conn)
 
     return cur, conn
+
+def data_calcs(cur, conn):
+    #join beers with a ph over 4.4 and abv over 8
+
+    #ph over 4.4
+    cur.execute("CREATE TABLE IF NOT EXISTS pHOver4 (id INTEGER PRIMARY KEY, name TEXT, abv REAL, ph REAL, contributed_by_id INTEGER)")
+    cur.execute("SELECT * FROM Beers WHERE ph > 4.3")
+    data = cur.fetchall()
+    for beer in data:
+        b = (beer[0], beer[1], beer[2], beer[3], beer[4])
+        cur.execute("INSERT OR IGNORE INTO pHOver4 VALUES (?,?,?,?,?)", b)
+
+    #join tables
+    #format: id, abv, ph, name
+    cur.execute("SELECT Beers.id, Beers.abv, pHOver4.ph, pHOver4.name FROM Beers JOIN pHOver4 ON Beers.abv > 8.0 AND Beers.id = pHOver4.id AND Beers.abv < 20.0")
+    beers = cur.fetchall()
+
+    conn.commit()
+
+    #visualize data
+    beers.sort(key = lambda x: x[1], reverse=True)
+    write_calcs(beers)
+    visualization(beers)
+
+    return cur, conn
+
+def create_contributed_db(data, cur, conn):
+    #create the table
+    cur.execute("""CREATE TABLE IF NOT EXISTS Contributers
+                 (id INTEGER PRIMARY KEY, name TEXT UNIQUE)""")
+
+    #load names
+    for item in data:
+        cur.execute("SELECT MAX(id) FROM Contributers")
+        counter = 1
+        if cur.fetchone()[0] is not None:
+            counter = cur.fetchone()
+        cur.execute("INSERT OR IGNORE INTO Contributers VALUES (?,?)", 
+                    (counter, item["contributed_by"]))
+
+    #commit changes
+    conn.commit()
 
 def load_data(page_num, cur, conn):
     #get data from API
@@ -31,24 +81,10 @@ def load_data(page_num, cur, conn):
 
     #data loading in: id, name, abv, ph, contributed by
     for beer in data:
-        cur.execute("SELECT id FROM Contributers WHERE name = ?", (beer["contributed_by"],))
+        cur.execute("SELECT id FROM Contributers WHERE name = ?",
+                     (beer["contributed_by"],))
         b = (beer["id"], beer["name"], beer["abv"], beer["ph"], cur.fetchone()[0])
         cur.execute("INSERT OR IGNORE INTO Beers VALUES (?,?,?,?,?)", b)
-
-    #commit changes
-    conn.commit()
-
-def create_contributed_db(data, cur, conn):
-    #create the table
-    cur.execute("CREATE TABLE IF NOT EXISTS Contributers (id INTEGER PRIMARY KEY, name TEXT UNIQUE)")
-
-    #load names
-    for item in data:
-        cur.execute("SELECT MAX(id) FROM Contributers")
-        counter = 1
-        if cur.fetchone()[0] is not None:
-            counter = cur.fetchone()
-        cur.execute("INSERT OR IGNORE INTO Contributers VALUES (?,?)", (counter, item["contributed_by"]))
 
     #commit changes
     conn.commit()
@@ -107,32 +143,6 @@ def write_calcs(beers):
         for beer in beers:
             #format: id, abv, ph, name
             f.write(f"{beer[3]}, pH: {beer[2]}, abv: {beer[1]}\n")
-
-
-def data_calcs(cur, conn):
-    #join beers with a ph over 4.4 and abv over 8
-
-    #ph over 4.4
-    cur.execute("CREATE TABLE IF NOT EXISTS pHOver4 (id INTEGER PRIMARY KEY, name TEXT, abv REAL, ph REAL, contributed_by_id INTEGER)")
-    cur.execute("SELECT * FROM Beers WHERE ph > 4.3")
-    data = cur.fetchall()
-    for beer in data:
-        b = (beer[0], beer[1], beer[2], beer[3], beer[4])
-        cur.execute("INSERT OR IGNORE INTO pHOver4 VALUES (?,?,?,?,?)", b)
-
-    #join tables
-    #format: id, abv, ph, name
-    cur.execute("SELECT Beers.id, Beers.abv, pHOver4.ph, pHOver4.name FROM Beers JOIN pHOver4 ON Beers.abv > 8.0 AND Beers.id = pHOver4.id AND Beers.abv < 20.0")
-    beers = cur.fetchall()
-
-    conn.commit()
-
-    #visualize data
-    beers.sort(key = lambda x: x[1], reverse=True)
-    write_calcs(beers)
-    visualization(beers)
-
-    return cur, conn
 
 def main():
     #create path for database
